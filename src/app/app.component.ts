afaf338c31e18d2c99f0f65129c3ae35f34308f9
mohanrajcse96 <mohan@sparkouttech.com>
2022-04-13 12:05:07 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import Web3 from 'web3';
+// Connect Wallet
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+
+// Set web3 and connector
+const web3 = new Web3(window['ethereum']);
 
 @Component({
   selector: 'app-root',
